87314d91ae13dbe9c0712f34b078804945d46437
Petar Marincheshki <pamarincheshki@gmail.com>
2026-08-25 12:16:29 +0200

    Fixed Round robin tests

--- cpu-scheduler-simulator/tests/test_round_robin.py
+++ cpu-scheduler-simulator/tests/test_round_robin.py
@@ -63,137 +63,68 @@ class TestRoundRobinScheduler:
                 RoundRobin_Scheduler(quantum=-1)
         
     
-class TestRoundRobinSimulator:
-    def test_process_shorter_than_quantum(self):
-        p1 = Process(process_id=1, arrival_time=0, burst_time=1)
+class TestRoundRobinScheduler:
 
-        workload = Workload([p1])
-        scheduler = RoundRobin_Scheduler(quantum=2)
-        simulator = Simulator(workload, scheduler)
-
-        simulator.run()
-        
-        assert p1.state == ProcessState.TERMINATED
-        assert p1.remaining_time == 0
-        assert p1.completion_time == 1
-        assert len(simulator.completed) == 1
-        
-    def test_process_equal_to_quantum(self):
-        p1 = Process(process_id=1, arrival_time=0, burst_time=2)
-
-        workload = Workload([p1])
-        scheduler = RoundRobin_Scheduler(quantum=2)
-        simulator = Simulator(workload, scheduler)
-
-        simulator.run()
-
-        assert p1.state == ProcessState.TERMINATED
-        assert p1.remaining_time == 0
-        assert p1.completion_time == 2
-        
-        assert simulator.ready_queue==[]
-        assert len(simulator.completed) == 1
-        
-    def test_process_longer_than_quantum(self):
-        p1 = Process(process_id=1, arrival_time=0, burst_time=5)
-
-        workload = Workload([p1])
-        scheduler = RoundRobin_Scheduler(quantum=2)
-        simulator = Simulator(workload, scheduler)
-
-        simulator.run()
-
-        assert p1.state == ProcessState.TERMINATED
-        assert p1.remaining_time == 0
-        assert p1.completion_time == 5
-        assert len(simulator.completed) == 1
-        
-    def test_multiple_processes_round_robin(self):
-        p1 = Process(process_id=1, arrival_time=0, burst_time=4)
-        p2 = Process(process_id=2, arrival_time=0, burst_time=4)
-
-        workload = Workload([p1, p2])
-        scheduler = RoundRobin_Scheduler(quantum=2)
-        simulator = Simulator(workload, scheduler)
-
-        simulator.run()
-
-        assert p1.state == ProcessState.TERMINATED
-        assert p2.state == ProcessState.TERMINATED
-
-        assert p1.remaining_time == 0
-        assert p2.remaining_time == 0
-
-        assert len(simulator.completed) == 2
-        assert simulator.cur_time == 8
-        
-    def test_same_arrival_time(self):
-        p1 = Process(process_id=1, arrival_time=0, burst_time=2)
-        p2 = Process(process_id=2, arrival_time=0, burst_time=2)
-        p3 = Process(process_id=3, arrival_time=0, burst_time=2)
-
-        workload = Workload([p1, p2, p3])
+    def test_quantum_one(self):
         scheduler = RoundRobin_Scheduler(quantum=1)
-        simulator = Simulator(workload, scheduler)
 
-        simulator.run()
-
-        assert all(
-            process.state == ProcessState.TERMINATED
-            for process in [p1, p2, p3]
+        p1 = Process(
+            process_id=1,
+            arrival_time=0,
+            burst_time=5
         )
 
-        assert len(simulator.completed) == 3
-        assert simulator.cur_time == 6
-        
-    def test_arrival_during_quantum(self):
-        p1 = Process(process_id=1, arrival_time=0, burst_time=5)
-        p2 = Process(process_id=2, arrival_time=1, burst_time=2)
+        scheduler.on_time_unit()
 
-        workload = Workload([p1, p2])
+        assert scheduler.should_preempt(p1, []) is True
+
+    def test_before_quantum_expires(self):
         scheduler = RoundRobin_Scheduler(quantum=3)
-        simulator = Simulator(workload, scheduler)
 
-        simulator.run()
+        p1 = Process(
+            process_id=1,
+            arrival_time=0,
+            burst_time=5
+        )
 
-        assert p1.state == ProcessState.TERMINATED
-        assert p2.state == ProcessState.TERMINATED
+        scheduler.on_time_unit()
+        scheduler.on_time_unit()
 
-        # P1 should finish at time 7
-        assert p1.completion_time == 7
+        assert scheduler.should_preempt(p1, []) is False
 
-        # P2 should get CPU after P1's first quantum
-        assert p2.start == 3
-        
-    def test_cpu_idle_before_first_process(self):
-        p1 = Process(process_id=1, arrival_time=5, burst_time=2)
+    def test_at_quantum_expiration(self):
+        scheduler = RoundRobin_Scheduler(quantum=3)
 
-        workload = Workload([p1])
+        p1 = Process(
+            process_id=1,
+            arrival_time=0,
+            burst_time=5
+        )
+
+        scheduler.on_time_unit()
+        scheduler.on_time_unit()
+        scheduler.on_time_unit()
+
+        assert scheduler.should_preempt(p1, []) is True
+
+    def test_reset(self):
         scheduler = RoundRobin_Scheduler(quantum=2)
-        simulator = Simulator(workload, scheduler)
 
-        simulator.run()
+        p1 = Process(
+            process_id=1,
+            arrival_time=0,
+            burst_time=5
+        )
 
-        assert p1.start == 5
-        assert p1.completion_time == 7
-        assert simulator.cur_time == 7
-        
-    def test_cpu_idle_between_processes(self):
-        p1 = Process(process_id=1, arrival_time=0, burst_time=1)
-        p2 = Process(process_id=2, arrival_time=10, burst_time=1)
+        scheduler.on_time_unit()
+        scheduler.on_time_unit()
 
-        workload = Workload([p1, p2])
-        scheduler = RoundRobin_Scheduler(quantum=2)
-        simulator = Simulator(workload, scheduler)
+        assert scheduler.should_preempt(p1, []) is True
 
-        simulator.run()
+        scheduler.reset()
 
-        assert p1.completion_time == 1
-        assert p2.start == 10
-        assert p2.completion_time == 11
-
-        assert simulator.cur_time == 11
-        assert len(simulator.completed) == 2
+        assert scheduler.time_used_in_slice == 0
+        assert scheduler.should_preempt(p1, []) is False
         
 class TestEdgeCaseTesting:
     def test_completion_order_shortest_process_finishes_first(self):
